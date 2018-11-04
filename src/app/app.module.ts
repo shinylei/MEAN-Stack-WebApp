64da55051687885894e8942f3cec35a5b4b1d6d7
shinylei <walei@umich.edu>
2018-11-03 21:04:03 -0400

    add posts using service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,20 @@ import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -25,6 +28,7 @@ import { HeaderComponent } from './header/header.component';
     MatInputModule,
     MatButtonModule,
     MatToolbarModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,17 +1,32 @@
-import { Component, Input} from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { Post } from '../post.model'
+import { PostsService } from '../posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy{
 // posts = [
 //   {title: 'First Post', content: 'This is first post'},
 //   {title: 'Second Post', content: 'This is second post'},
 //   {title: 'Third Post', content: 'This is third post'},
 // ];
-@Input()
+constructor(public postsService : PostsService) {}
+
 posts :Post[] = [];
+private postsSub : Subscription;
+
+ngOnInit() {
+  this.posts = this.postsService.getPost();
+  this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
+    this.posts = posts;
+  });
+}
+
+ngOnDestroy() {
+  this.postsSub.unsubscribe();
+}
 }
