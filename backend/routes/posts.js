9cc321464f2b324937baab336e77db82d367b9cd
shinylei <walei@umich.edu>
2018-11-07 22:00:31 -0500

    add middleware for authentication

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 
 const Post = require("../models/post");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
@@ -62,7 +63,7 @@ router.get('/:id', (req, res, next) => {
     })
 });
 
-router.post("", multer({storage: storage}).single("image"),(req, res, next) => {
+router.post("",checkAuth, multer({storage: storage}).single("image"),(req, res, next) => {
     const url = req.protocol + '://' + req.get('host');
     const post = new Post({
         title: req.body.title,
@@ -81,7 +82,7 @@ router.post("", multer({storage: storage}).single("image"),(req, res, next) => {
     
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', checkAuth,(req, res, next) => {
     // console.log(req.params.id);
     Post.deleteOne({_id: req.params.id}).then(result => {
         console.log(result);
@@ -89,7 +90,7 @@ router.delete('/:id', (req, res, next) => {
     });
 });
 
-router.put('/:id', multer({storage: storage}).single("image"), (req, res, next) => {
+router.put('/:id',checkAuth, multer({storage: storage}).single("image"), (req, res, next) => {
     let imagePath = req.body.imagePath;
     if (req.file) {
         const url = req.protocol + '://' + req.get('host');
